1935c8f20366defbb6209e907c97153aeddf7360
kyh <yh0921k@gmail.com>
2019-12-24 11:39:06 +0900

    update v12

---
+++ src/main/java/com/eomcs/lms/domain/Board.java
@@ -0,0 +1,11 @@
+package com.eomcs.lms.domain;
+
+import java.util.Date;
+
+public class Board {
+  public int    postNum;
+  public String postContent;
+  public Date   writeDay;
+  public int    viewCount;
+  public String writer;
+}

0e671a6550a1cf5ed41b198c26028e294573576c
kyh <yh0921k@gmail.com>
2019-12-27 14:56:34 +0900

    update v14

---
+++ v14/src/main/java/com/eomcs/lms/domain/User.java
@@ -0,0 +1,13 @@
+package com.eomcs.lms.domain;
+
+import java.util.Date;
+
+public class User {
+  public int    personNum;
+  public String personName;
+  public String PhoneNum;
+  public String picture;
+  public String passwd;
+  public String email;
+  public Date   joinDay;
+}

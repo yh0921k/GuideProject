ba6edfda611b029191f0034a2ba50902dbab4ff8
kyh <yh0921k@gmail.com>
2020-01-14 15:36:35 +0900

    update v23_03

---
+++ step/v23_03/src/main/java/com/eomcs/util/List.java
@@ -0,0 +1,13 @@
+package com.eomcs.util;
+
+public interface List<E> {
+  
+  public abstract void add(E e);
+  public abstract void add(int index, E e);
+  public abstract E get(int index);
+  public abstract E set(int index, E e);
+  public abstract E remove(int index);
+  public abstract Object[] toArray();
+  public abstract E[] toArray(E[] arr);
+  int size();
+}

5ecaab1ecfa5dd8d177a09073b30624f2cb03c93
kyh <yh0921k@gmail.com>
2020-01-15 17:18:19 +0900

    update v24_01

---
+++ src/main/java/com/eomcs/util/StackIterator.java
@@ -0,0 +1,19 @@
+package com.eomcs.util;
+
+public class StackIterator<E> implements Iterator<E> {
+  Stack<E> stack;
+  
+  public StackIterator(Stack<E> stack) {
+    this.stack = stack.clone();
+  }
+  
+  @Override
+  public boolean hasNext() {
+    return !stack.empty();
+  }
+  
+  @Override
+  public E next() {
+    return stack.pop();
+  }
+}

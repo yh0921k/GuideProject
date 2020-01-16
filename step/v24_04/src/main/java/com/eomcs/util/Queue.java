dfbfb72e3fadc8b674b9e3efd79d3ba72311efd6
kyh <yh0921k@gmail.com>
2020-01-16 12:50:04 +0900

    update v24_02~v24_05 and modified README.md in v24_01

--- step/v24_04/src/main/java/com/eomcs/util/Queue.java
+++ step/v24_04/src/main/java/com/eomcs/util/Queue.java
@@ -1,15 +1,15 @@
 package com.eomcs.util;
 
 public class Queue<E> extends LinkedList<E> implements Cloneable {
-
+  
   public void offer(E value) {
     this.add(value);
   }
-
+  
   public E poll() {
     return this.remove(0);
   }
-
+  
   @Override
   public Queue<E> clone() {
     try {
@@ -22,24 +22,28 @@ public class Queue<E> extends LinkedList<E> implements Cloneable {
       return null;
     }
   }
-
+  
   public Iterator<E> iterator() {
-    return new Iterator<E>() { 
-      Queue<E> queue;
-
-      {
-        this.queue = (Queue<E>)Queue.this.clone();
+    
+    class QueueIterator<T> implements Iterator<T> {
+      Queue<T> queue;
+      
+      @SuppressWarnings("unchecked")
+      public QueueIterator() {
+        this.queue = (Queue<T>)Queue.this.clone();
       }
-
+      
       @Override
       public boolean hasNext() {
         return queue.size() > 0;
       }
-
+      
       @Override
-      public E next() {
+      public T next() {
         return queue.poll();
       }
-    };
+    }
+    
+    return new QueueIterator<>();
   }
 }

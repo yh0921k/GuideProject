df053264601f2a32914771d5d86cfe0725ea1f1d
kyh <yh0921k@gmail.com>
2020-02-25 18:21:17 +0900

    update v38_04 in server project

---
+++ guide-project-server/step/v38_04/src/main/java/com/eomcs/lms/servlet/MemberUpdateServlet.java
@@ -0,0 +1,49 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import com.eomcs.lms.dao.MemberDao;
+import com.eomcs.lms.domain.Member;
+import com.eomcs.util.Prompt;
+
+public class MemberUpdateServlet implements Servlet {
+
+  MemberDao memberDao;
+
+  public MemberUpdateServlet(MemberDao memberDao) {
+    this.memberDao = memberDao;
+  }
+
+  @Override
+  public void service(Scanner in, PrintStream out) throws Exception {
+    out.println("번호 : \n!{}!");
+    int no = Integer.parseInt(in.nextLine());
+
+    Member oldMember;
+    oldMember = memberDao.findByNo(no);
+    if (oldMember == null) {
+      out.println("해당 번호의 수업이 없습니다.");
+      return;
+    }
+
+    Member newMember = new Member();
+    newMember.setNo(oldMember.getNo());
+    newMember.setName(Prompt.getString(in, out, String.format("이름(%s)", oldMember.getName()),
+        oldMember.getName()));
+    newMember.setEmail(Prompt.getString(in, out, String.format("이메일(%s)", oldMember.getEmail()),
+        oldMember.getEmail()));
+    newMember.setPassword(Prompt.getString(in, out,
+        String.format("암호(%s)", oldMember.getPassword()), oldMember.getPassword()));
+    newMember.setPhoto(Prompt.getString(in, out, String.format("사진(%s)", oldMember.getPhoto()),
+        oldMember.getPhoto()));
+    newMember.setTel(
+        Prompt.getString(in, out, String.format("전화(%s)", oldMember.getTel()), oldMember.getTel()));
+
+    if (memberDao.update(newMember) > 0) {
+      out.println("회원을 변경했습니다.");
+
+    } else {
+      out.println("회원 업데이트 실패");
+    }
+  }
+}

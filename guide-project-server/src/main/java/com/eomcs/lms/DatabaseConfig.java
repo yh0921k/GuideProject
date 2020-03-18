31806f8f4f49067add636d0d8e9112cd17837a1b
kyh <yh0921k@gmail.com>
2020-03-18 14:26:33 +0900

    update v49_02 in server project

---
+++ guide-project-server/src/main/java/com/eomcs/lms/DatabaseConfig.java
@@ -0,0 +1,60 @@
+package com.eomcs.lms;
+
+import java.io.IOException;
+import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
+
+// Spring IoC 컨테이너가 이 클래스를 JavaConfig로 자동 인식하게 하려면 아래와 같이 사용
+// 단, 이 클래스가 @ComponentScan에서 지정한 패키지 안에 있어야 한다.
+@Configuration
+
+// Spring IoC Container에서 사용할 Properties 파일 로딩
+@PropertySource("classpath:com/eomcs/lms/conf/jdbc.properties")
+public class DatabaseConfig {
+
+  // @PropertySource 로 로딩한 .proerties 파일의 값을 사용하고 싶다면, 다음 어노테이션을 인스턴스 필드 앞에 붙인다.
+  // Spring IoC 컨테이너가 이 클래스의 객체를 생성할 때 해당 필드에 프로퍼티 값을 자동으로 주입할 것이다.
+  @Value("${jdbc.driver}")
+  String jdbcDriver;
+
+  @Value("${jdbc.url}")
+  String jdbcUrl;
+
+  @Value("${jdbc.username}")
+  String jdbcUsername;
+
+  @Value("${jdbc.password}")
+  String jdbcPassword;
+
+  public DatabaseConfig() throws IOException {
+    // Mybatis 설정 파일을 로딩할 때 사용할 입력 스트림 준비
+    System.out.println("Create DatabaseConfig");
+  }
+
+  // Spring IoC Container에 수동으로 등록할 객체는 메서드를 만들어 리턴한다.
+  // > 단 메서드 선언분에 @Bean 어노테이션을 붙여야 한다.
+  // String Ioc Container에 수동으로 객체를 등ㄹ고하고 싶다면, 해당 객체를 만들어 주느 팩토리 메서들르 정의해야 한다.
+  // > 그래야만 Spring IoC Container는 이 메서드를 호출하고 결과값을 저장한다.
+
+
+  @Bean
+  public DataSource dataSource() {
+    DriverManagerDataSource ds = new DriverManagerDataSource();
+    ds.setDriverClassName(jdbcDriver);
+    ds.setUrl(jdbcUrl);
+    ds.setUsername(jdbcUsername);
+    ds.setPassword(jdbcPassword);
+    return ds;
+  }
+
+  @Bean
+  public PlatformTransactionManager transactionManager(DataSource dataSource) {
+    return new DataSourceTransactionManager(dataSource);
+  }
+}
---
+++ guide-project-server/step/v49_02/src/main/java/com/eomcs/lms/servlet/LessonUpdateServlet.java
@@ -0,0 +1,57 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.domain.Lesson;
+import com.eomcs.lms.service.LessonService;
+import com.eomcs.util.Prompt;
+import com.eomcs.util.RequestMapping;
+
+@Component
+public class LessonUpdateServlet {
+
+  LessonService lessonService;
+
+  public LessonUpdateServlet(LessonService lessonService) {
+    this.lessonService = lessonService;
+  }
+
+  @RequestMapping("/lesson/update")
+  public void service(Scanner in, PrintStream out) throws Exception {
+    int no = Prompt.getInt(in, out, "번호 : ");
+
+    Lesson oldLesson;
+    oldLesson = lessonService.get(no);
+    if (oldLesson == null) {
+      out.println("해당 번호의 수업이 없습니다.");
+      return;
+    }
+
+    Lesson newLesson = new Lesson();
+    newLesson.setNo(oldLesson.getNo());
+    newLesson.setTitle(Prompt.getString(in, out, String.format("제목(%s) ; ", oldLesson.getTitle())));
+
+    newLesson.setDescription(
+        Prompt.getString(in, out, String.format("설명(%s) : ", oldLesson.getDescription())));
+
+    newLesson.setStartDate(
+        Prompt.getDate(in, out, String.format("시작일(%s) : ", oldLesson.getStartDate())));
+
+    newLesson
+        .setEndDate(Prompt.getDate(in, out, String.format("종료일(%s) : ", oldLesson.getEndDate())));
+
+    newLesson.setTotalHours(
+        Prompt.getInt(in, out, String.format("총수업시간(%s) : ", oldLesson.getTotalHours())));
+
+    newLesson.setDayHours(
+        Prompt.getInt(in, out, String.format("일수업시간(%s) : ", oldLesson.getDayHours())));
+
+    if (lessonService.update(newLesson) > 0) {
+      out.println("수업을 변경했습니다.");
+
+    } else {
+      out.println("수업 업데이트 실패");
+    }
+  }
+}

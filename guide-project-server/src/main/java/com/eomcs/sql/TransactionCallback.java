b1e769080598a548e9f85d83163d56c5c4551aba
kyh <yh0921k@gmail.com>
2020-03-18 12:57:54 +0900

    update v49_01 in server project

--- guide-project-server/src/main/java/com/eomcs/sql/TransactionCallback.java
+++
@@ -1,5 +0,0 @@
-package com.eomcs.sql;
-
-public interface TransactionCallback {
-  Object doInTransaction() throws Exception;
-}
---
+++ guide-project-server/step/v49_01/src/main/java/com/eomcs/lms/servlet/BoardUpdateServlet.java
@@ -0,0 +1,44 @@
+package com.eomcs.lms.servlet;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+import org.springframework.stereotype.Component;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.service.BoardService;
+import com.eomcs.util.Prompt;
+import com.eomcs.util.RequestMapping;
+
+@Component
+public class BoardUpdateServlet {
+
+  BoardService boardService;
+
+  public BoardUpdateServlet(BoardService boardService) {
+    this.boardService = boardService;
+  }
+
+  @RequestMapping("/board/update")
+  public void service(Scanner in, PrintStream out) throws Exception {
+
+    int no = Prompt.getInt(in, out, "번호 : ");
+
+    Board oldBoard;
+    oldBoard = boardService.get(no);
+    if (oldBoard == null) {
+      out.println("해당 번호의 게시글이 없습니다.");
+      return;
+    }
+
+    Board newBoard = new Board();
+    newBoard.setNo(no);
+    newBoard.setTitle(Prompt.getString(in, out,
+        String.format("제목(%s) : \n", oldBoard.getTitle(), oldBoard.getTitle())));
+
+    if (boardService.update(newBoard) > 0) {
+      out.println("게시물을 변경했습니다.");
+
+    } else {
+      out.println("게시물 업데이트 실패");
+    }
+  }
+}

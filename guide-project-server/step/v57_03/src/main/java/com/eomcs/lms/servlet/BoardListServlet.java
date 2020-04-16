df9c649e155c8e68e4bfd6107a02f65387af5661
Yonghwi Kim <yh0921k@gmail.com>
2020-04-16 19:02:15 +0900

    update v57_03 in server project

---
+++ guide-project-server/step/v57_03/src/main/java/com/eomcs/lms/servlet/BoardListServlet.java
@@ -0,0 +1,40 @@
+package com.eomcs.lms.servlet;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.service.BoardService;
+
+@WebServlet("/board/list")
+public class BoardListServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void service(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    try {
+
+      ServletContext servletContext = request.getServletContext();
+      ApplicationContext iocContainer =
+          (ApplicationContext) servletContext.getAttribute("iocContainer");
+      BoardService boardService = iocContainer.getBean(BoardService.class);
+
+      List<Board> boards = boardService.list();
+      request.setAttribute("list", boards);
+      response.setContentType("text/html;charset=UTF-8");
+      request.getRequestDispatcher("/board/list.jsp").include(request, response);
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      request.setAttribute("url", "list");
+      request.getRequestDispatcher("/error").forward(request, response);
+    }
+  }
+}

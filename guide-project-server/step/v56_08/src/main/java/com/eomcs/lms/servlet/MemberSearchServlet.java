15aada54d32cba12c4dfbb0edf1e5a20661a2539
Yonghwi Kim <yh0921k@gmail.com>
2020-04-16 10:57:57 +0900

    update v56_08 in server project

--- guide-project-server/step/v56_08/src/main/java/com/eomcs/lms/servlet/MemberSearchServlet.java
+++ guide-project-server/step/v56_08/src/main/java/com/eomcs/lms/servlet/MemberSearchServlet.java
@@ -2,6 +2,7 @@ package com.eomcs.lms.servlet;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,8 +13,8 @@ import org.springframework.context.ApplicationContext;
 import com.eomcs.lms.domain.Member;
 import com.eomcs.lms.service.MemberService;
 
-@WebServlet("/member/detail")
-public class MemberDetailServlet extends HttpServlet {
+@WebServlet("/member/search")
+public class MemberSearchServlet extends HttpServlet {
   private static final long serialVersionUID = 1L;
 
   @Override
@@ -28,35 +29,30 @@ public class MemberDetailServlet extends HttpServlet {
           (ApplicationContext) servletContext.getAttribute("iocContainer");
       MemberService memberService = iocContainer.getBean(MemberService.class);
 
-      int no = Integer.parseInt(request.getParameter("no"));
-      Member member = memberService.get(no);
-
       request.getRequestDispatcher("/header").include(request, response);
-      out.println("<h1>회원 상세정보</h1>");
+      out.println("  <h1>회원 검색 결과</h1>");
+      out.println("  <table border='1'>");
+      out.println("  <tr>");
+      out.println("    <th>번호</th>");
+      out.println("    <th>이름</th>");
+      out.println("    <th>이메일</th>");
+      out.println("    <th>전화</th>");
+      out.println("    <th>등록일</th>");
+      out.println("  </tr>");
 
-      if (member != null) {
-        out.println("<form action='update' method='post' enctype='multipart/form-data'>");
-        out.printf("<img src='../upload/member/%s' height='80'><br>\n", //
-            member.getPhoto());
-        out.printf("번호: <input name='no' type='text' readonly value='%d'><br>\n", //
-            member.getNo());
-        out.printf("이름: <input name='name' type='text' value='%s'><br>\n", //
-            member.getName());
-        out.printf("이메일: <input name='email' type='email' value='%s'><br>\n", //
-            member.getEmail());
-        out.println("암호: <input name='password' type='password'><br>");
-        out.printf("사진: <input name='photo' type='file'><br>\n", //
-            member.getPhoto());
-        out.printf("전화: <input name='tel' type='tel' value='%s'><br>\n", //
-            member.getTel());
-        out.println("<p><button>변경</button>");
-        out.printf("<a href='delete?no=%d'>삭제</a></p>\n", //
-            member.getNo());
-        out.println("</form>");
-
-      } else {
-        out.println("<p>해당 번호의 회원이 없습니다.</p>");
+      String keyword = request.getParameter("keyword");
+      List<Member> members = memberService.search(keyword);
+      for (Member m : members) {
+        out.printf("  <tr>"//
+            + "<td>%d</td> "//
+            + "<td><a href='detail?no=%d'>%s</a></td> "//
+            + "<td>%s</td> "//
+            + "<td>%s</td>"//
+            + "<td>%s</td>"//
+            + "</tr>\n", //
+            m.getNo(), m.getNo(), m.getName(), m.getEmail(), m.getTel(), m.getRegisteredDate());
       }
+      out.println("</table>");
       request.getRequestDispatcher("/footer").include(request, response);
     } catch (Exception e) {
       request.setAttribute("error", e);

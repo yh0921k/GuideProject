b73d55d012f3a109c19db1574b4d92f86fc42335
Yonghwi Kim <yh0921k@gmail.com>
2020-04-16 15:46:07 +0900

    update v57_02 in server project

---
+++ guide-project-server/step/v57_02/src/main/java/com/eomcs/lms/servlet/BoardAddServlet.java
@@ -0,0 +1,52 @@
+package com.eomcs.lms.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.springframework.context.ApplicationContext;
+import com.eomcs.lms.domain.Board;
+import com.eomcs.lms.service.BoardService;
+
+@WebServlet("/board/add")
+public class BoardAddServlet extends HttpServlet {
+  private static final long serialVersionUID = 1L;
+
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+    response.setContentType("text/html;charset=UTF-8");
+    request.getRequestDispatcher("/board/form.jsp").include(request, response);
+  }
+
+  @Override
+  protected void doPost(HttpServletRequest request, HttpServletResponse response)
+      throws ServletException, IOException {
+
+    try {
+      request.setCharacterEncoding("UTF-8");
+
+      ServletContext servletContext = request.getServletContext();
+      // ServletContext servletContext = this.getServletContext(); // 가능
+      // ServletContext servletContext = getServletContext(); // 가능
+      ApplicationContext iocContainer =
+          (ApplicationContext) servletContext.getAttribute("iocContainer");
+      BoardService boardService = iocContainer.getBean(BoardService.class);
+
+      Board board = new Board();
+      board.setTitle(request.getParameter("title"));
+      if (boardService.add(board) > 0)
+        response.sendRedirect("list");
+      else
+        throw new Exception("게시글을 추가할 수 없습니다.");
+
+    } catch (Exception e) {
+      request.setAttribute("error", e);
+      request.setAttribute("url", "list");
+      request.getRequestDispatcher("/error").forward(request, response);
+    }
+  }
+}

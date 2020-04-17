a9cacf51627308a6d010684ed4ddbe4c686d1668
Yonghwi Kim <yh0921k@gmail.com>
2020-04-17 14:21:50 +0900

    update v58_02 in server project

---
+++ guide-project-server/step/v58_02/src/main/java/com/eomcs/lms/service/MemberService.java
@@ -0,0 +1,20 @@
+package com.eomcs.lms.service;
+
+import java.util.List;
+import com.eomcs.lms.domain.Member;
+
+public interface MemberService {
+  int add(Member member) throws Exception;
+
+  List<Member> list() throws Exception;
+
+  Member get(int no) throws Exception;
+
+  Member get(String email, String password) throws Exception;
+
+  List<Member> search(String keyword) throws Exception;
+
+  int delete(int no) throws Exception;
+
+  int update(Member member) throws Exception;
+}
